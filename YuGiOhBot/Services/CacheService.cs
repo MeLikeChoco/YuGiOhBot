a0a9958f661e7b3c04af1569b24702fe6f786157
MeLikeChoco <melikechoco@gmail.com>
2017-03-22 00:59:33 -0400

    working on cache service

---
+++ YuGiOhBot/Services/CacheService.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//for those wondering why i couldnt just put each cache in its respect service class
+//cause im picky with my using keyword, i didnt want yugiohservice to use Discord
+
+namespace YuGiOhBot.Services
+{
+    public class CacheService
+    {
+
+        public ConcurrentDictionary<string, Embed> _yuGiOhCardCache { get; set; }
+
+    }
+}
