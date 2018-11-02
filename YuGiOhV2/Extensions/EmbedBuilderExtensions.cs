e65599aab12d988bcee0b3eda08d9978792c1130
Edgar Chang <melikechoco@gmail.com>
2018-11-02 02:59:46 -0400

    v6.3.3
    Moved adding prices to cards in embeds to an extension for cleaner use

--- YuGiOhV2/Extensions/EmbedBuilderExtensions.cs
+++ YuGiOhV2/Extensions/EmbedBuilderExtensions.cs
@@ -1,9 +1,12 @@
 ﻿using Discord;
+using Force.DeepCloner;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YuGiOhV2.Objects.Deserializers;
+using YuGiOhV2.Services;
 
 namespace YuGiOhV2.Extensions
 {
@@ -14,6 +17,89 @@ namespace YuGiOhV2.Extensions
 
         public static EmbedBuilder WithRandomColor(this EmbedBuilder builder)
             => builder.WithColor(_rand.NextColor());
+
+        public static async Task<EmbedBuilder> WithPrices(this EmbedBuilder embed, bool minimal, Web web, TimeSpan? searchTime = null)
+        {
+
+            var clone = embed.DeepClone();
+            TimeSpan time;
+
+            if (searchTime != null)
+            {
+
+                time = searchTime.Value;
+                var rounded = Math.Round(time.TotalSeconds, 5, MidpointRounding.ToEven).ToString("0.00000");
+
+                clone.Footer.WithText($"Search time: {rounded} seconds");
+
+            }
+
+            if (minimal)
+            {
+
+                clone.ThumbnailUrl = clone.ImageUrl;
+                clone.ImageUrl = null;
+
+            }
+            else
+            {
+
+                string realName;
+
+                if (clone.Description.Contains("Real Name"))
+                {
+
+                    var indexOne = clone.Description.IndexOf(':');
+                    var indexTwo = clone.Description.IndexOf("**Format");
+                    realName = clone.Description.Substring(indexOne, indexTwo).Trim();
+
+                }
+                else
+                    realName = clone.Author.Name;
+
+                var response = await web.GetPrices(clone.Author.Name, realName);
+
+                if (response.Data != null)
+                {
+
+                    IEnumerable<Datum> prices;
+
+                    if (response.Data.Count >= 4)
+                    {
+
+                        clone.AddField("Prices", "**Showing the first 3 prices due to too many to show**");
+
+                        prices = response.Data.Take(3);
+
+                    }
+                    else
+                        prices = response.Data;
+
+                    foreach (Datum info in prices)
+                    {
+
+                        if (string.IsNullOrEmpty(info.PriceData.Message))
+                        {
+
+                            clone.AddField(info.Name,
+                                $"Rarity: {info.Rarity}\n" +
+                                $"Average Price: {info.PriceData.Data.Prices.Average.ToString("0.00")}");
+
+                        }
+                        else
+                            clone.AddField(info.Name, info.PriceData.Message);
+
+                    }
+
+                }
+                else
+                    clone.AddField("Prices", "**No prices to show for this card!**");
+
+            }
+
+            return clone;
+
+        }
 
     }
 }
--- YuGiOhV2/Services/Chat.cs
+++ YuGiOhV2/Services/Chat.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using YuGiOhV2.Extensions;
 using YuGiOhV2.Objects.Comparers;
 using YuGiOhV2.Objects.Deserializers;
 
@@ -111,7 +112,7 @@ namespace YuGiOhV2.Services
                         try
                         {
 
-                            await channel.SendMessageAsync("", embed: (await EditEmbed(embed, minimal, time)).Build());
+                            await channel.SendMessageAsync("", embed: (await embed.WithPrices(minimal, _web, time)).Build());
 
                         }
                         catch { AltConsole.Write("Service", "Chat", "No permission to send message"); }
@@ -124,89 +125,7 @@ namespace YuGiOhV2.Services
 
         } //look at em brackets
 
-        public static async Task<EmbedBuilder> EditEmbed(EmbedBuilder embed, bool minimal, TimeSpan? searchTime = null)
-        {
-
-            var clone = embed.DeepClone();
-            TimeSpan time;
-
-            if (searchTime != null)
-            {
-
-                time = searchTime.Value;
-                var rounded = Math.Round(time.TotalSeconds, 5, MidpointRounding.ToEven).ToString("0.00000");
-
-                clone.Footer.WithText($"Search time: {rounded} seconds");
-
-            }
-
-            if (minimal)
-            {
-
-                clone.ThumbnailUrl = clone.ImageUrl;
-                clone.ImageUrl = null;
-
-            }
-            else
-            {
-
-                string realName;
-
-                if (clone.Description.Contains("Real Name"))
-                {
-
-                    var indexOne = clone.Description.IndexOf(':');
-                    var indexTwo = clone.Description.IndexOf("**Format");
-                    realName = clone.Description.Substring(indexOne, indexTwo).Trim();
-
-                }
-                else
-                    realName = clone.Author.Name;
-
-                var response = await _web.GetPrices(clone.Author.Name, realName);
-
-                if (response.Data != null)
-                {
-
-                    IEnumerable<Datum> prices;
-
-                    if (response.Data.Count >= 4)
-                    {
-
-                        clone.AddField("Prices", "**Showing the first 3 prices due to too many to show**");
-
-                        prices = response.Data.Take(3);
-
-                    }
-                    else
-                        prices = response.Data;
-
-                    foreach (Datum info in prices)
-                    {
-
-                        if (string.IsNullOrEmpty(info.PriceData.Message))
-                        {
-
-                            clone.AddField(info.Name,
-                                $"Rarity: {info.Rarity}\n" +
-                                $"Average Price: {info.PriceData.Data.Prices.Average.ToString("0.00")}");
-
-                        }
-                        else
-                            clone.AddField(info.Name, info.PriceData.Message);
-
-                    }
-
-                }
-                else
-                    clone.AddField("Prices", "**No prices to show for this card!**");
-
-            }
-
-            return clone;
-
-        }
-
+        #region Levenshtein Distance
         //dont even ask me what the fuck im doing
         private unsafe int YetiLevenshtein(string s1, string s2)
         {
@@ -413,6 +332,7 @@ namespace YuGiOhV2.Services
             }
             return 0;
         }
+        #endregion Levenshtein Distance
 
     }
 }
