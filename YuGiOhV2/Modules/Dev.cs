fa3ec546485197ceef23f6dd58ed248edb0e5cf1
Edgar Chang <melikechoco@gmail.com>
2019-02-04 16:37:31 -0500

    v7.5
    Added dev module

---
+++ YuGiOhV2/Modules/Dev.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YuGiOhV2.Objects.Attributes;
+
+namespace YuGiOhV2.Modules
+{
+    [RequireContext(ContextType.Guild)]
+    [RequireChannel(541938684438511616)]
+    public class Dev : MainBase
+    {
+
+        [Command("")]
+
+    }
+}
