e0bbfdfcb8452c684d6cd5b647d2dcbc0e3da110
Edgar Chang <melikechoco@gmail.com>
2019-02-06 15:23:44 -0500

    v7.5.5
    Added DoIf extension for any object, pretty useful

---
+++ YuGiOhScraper/Extensions/GeneralExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGiOhScraper.Extensions
+{
+    public static class GeneralExtensions
+    {
+
+        public static void DoIf<T>(this T x, Predicate<T> conditions, Action<T> work)
+        {
+
+            if (conditions.Invoke(x))
+                work.Invoke(x);
+
+        }
+
+    }
+}
---
+++ YuGiOhV2/Extensions/GeneralExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGiOhV2.Extensions
+{
+    public static class GeneralExtensions
+    {
+
+        public static void DoIf<T>(this T x, Predicate<T> conditions, Action<T> work)
+        {
+
+            if (conditions.Invoke(x))
+                work.Invoke(x);
+
+        }
+
+    }
+}
