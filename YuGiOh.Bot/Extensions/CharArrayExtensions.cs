07cb5503eeaab9b529fc4e88abb38a14a14e4ab0
Edgar Chang <melikechoco@gmail.com>
2022-01-27 14:26:49 -0500

    Created char[] to string extension

---
+++ YuGiOh.Bot/Extensions/CharArrayExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGiOh.Bot.Extensions
+{
+    public static class CharArrayExtensions
+    {
+
+        public static string ToString(this char[] array)
+            => new(array);
+
+        public static string ToString(this IEnumerable<char> array)
+            => ToString(array.ToArray());
+
+    }
+}
