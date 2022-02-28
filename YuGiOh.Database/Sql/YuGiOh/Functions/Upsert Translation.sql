213752347e8c13be214f78755896f451c203cbc7
Edgar Chang <melikechoco@gmail.com>
2022-02-28 05:28:34 -0500

    Added checks to Upsert Translation.sql to reduce PostgreSQL autovacuum triggers

--- YuGiOh.Database/Sql/YuGiOh/Functions/Upsert Translation.sql
+++ YuGiOh.Database/Sql/YuGiOh/Functions/Upsert Translation.sql
@@ -1,4 +1,5 @@
-create function upsert_translation(cardid integer, language character varying, name character varying, lore character varying) returns integer
+create function upsert_translation(cardid integer, language character varying, name character varying,
+                                   lore character varying) returns integer
     language plpgsql
 as
 $$
@@ -6,12 +7,15 @@ declare
     result integer;
 BEGIN
 
-    insert into translations(cardid, language, name, lore) values(cardid, language, name, lore)
+    insert into translations(cardid, language, name, lore)
+    values (cardid, language, name, lore)
     on conflict on constraint cardid_language_pair_unique
-        do
-            update set
-                       name = excluded.name,
-                       lore = excluded.lore
+        do update
+        set name = excluded.name,
+            lore = excluded.lore
+    where translations.cardid = excluded.cardid
+      and translations.language = excluded.language
+      and (translations.name != excluded.name or translations.lore != excluded.lore)
     returning id into result;
 
     return result;
